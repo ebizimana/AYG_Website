900d2ced49d68fd3a724029c661f1225c0a7a71d
Elie Bizimana <ebizimana@liberty.edu>
2018-09-22 09:07:37 -0400

    First Commit:

---
+++ Assignment.js
@@ -0,0 +1,42 @@
+class Assignment {
+  constructor() {
+  }
+  // Essential variables
+  var name:       //Name of the the Asssignment
+  var graded:      //a boll varible to check if the assignment is graded or
+  var grade:       // if graded is true then this varible has a number if graded is false then this varible is not set.
+  var total:       // the total
+
+  // Constant variables
+  var deadLine:     //The deadline of the Assignment this is optional for now
+  var category:    // This puts the Assignment in a group with other assignments so they can be easly manunupulated as whole.
+
+  // Functions
+
+  // Adds one Assignment
+  function addAssignment() {
+
+  }
+
+  // list Assignment
+  function list() {
+    // List all Assignment
+    // List only seletected Assignment
+    // List assignment in a choosen category
+  }
+
+  // Deletes Assignments
+  function delete() {
+    // delete all assignments
+    // delete only selected assignments
+    // delete assignments in the category
+  }
+
+  // Edit Assignments
+  function edit() {
+    // edit selected assignment
+
+  }
+
+
+}
---
+++ Class.js
@@ -0,0 +1,41 @@
+class Class  {
+  constructor() {
+
+  }
+
+  // Varribles
+  var name:    // The name of the class
+  var teacher:  // The name of the teacher
+  var totalAssignments:  // the total number of the Assignments
+  var tag:    // All the different category (eg: quiz, exam...) the class has
+  var currentLetterGrade:   // The grade the student has now
+
+
+
+  // Functions
+
+  //This wil add a class
+  function addClass() {
+
+  }
+
+  // This list all the classes
+  function listClasses() {
+
+  }
+
+  // This will delete a class
+  function deleteClass() {
+
+  }
+
+  // This will delete all classes
+  function deleteAllClasses() {
+
+  }
+
+  // This is will update a class
+  function editClass() {
+
+  }
+}
